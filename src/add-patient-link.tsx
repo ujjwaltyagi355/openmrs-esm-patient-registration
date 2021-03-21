843b6253bb48e4936a1e4ac450f909be221e009b
ujjwaltyagi355 <ujjwaltyagi355@gmail.com>
2021-03-22 01:15:40 +0530

    Changed configurable link to navigate to

--- src/add-patient-link.tsx
+++ src/add-patient-link.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { ConfigurableLink } from '@openmrs/esm-framework';
+import { navigate } from '@openmrs/esm-framework';
 import Add20 from '@carbon/icons-react/es/add/20';
 import { HeaderGlobalAction } from 'carbon-components-react/es/components/UIShell';
-import './add-patient-link.css';
 export default function Root() {
   return (
-    <HeaderGlobalAction aria-label="Add" aria-labelledby="Add Patient" name="AddPatientIcon">
-      <ConfigurableLink to="${openmrsSpaBase}/patient-registration">
-        <Add20 className="add-patient-color" />
-      </ConfigurableLink>
+    <HeaderGlobalAction
+      aria-label="Add"
+      aria-labelledby="Add Patient"
+      name="AddPatientIcon"
+      onClick={() => {
+        navigate({ to: '${openmrsSpaBase}/patient-registration' });
+      }}>
+      <Add20 />
     </HeaderGlobalAction>
   );
 }
